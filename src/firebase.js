c271a1777424e31d2ab00e05f83fdeb203fac172
shevkar-shubhra <shubhrashevkar@gmail.com>
2024-10-24 18:32:52 +0530

    Commit

--- src/firebase.js
+++ src/firebase.js
@@ -7,18 +7,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log(import.meta.env.VITE_API_KEY);
 const firebaseConfig = {
-    apiKey: "AIzaSyDB18pBJcxhiHuUPoOeXj_5Ong4ToIA1nQ",
-    authDomain: "resource-portal-3f3e1.firebaseapp.com",
-    projectId: "resource-portal-3f3e1",
-    storageBucket: "resource-portal-3f3e1.appspot.com",
-    messagingSenderId: "188718695414",
-    appId: "1:188718695414:web:cd426120c85c1aa32b7dd7",
-    measurementId: "G-3LEFQ7EPY1"
-  };
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "resource-portal-3f3e1.firebaseapp.com",
+  projectId: "resource-portal-3f3e1",
+  storageBucket: "resource-portal-3f3e1.appspot.com",
+  messagingSenderId: "188718695414",
+  appId: "1:188718695414:web:cd426120c85c1aa32b7dd7",
+  measurementId: "G-3LEFQ7EPY1"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-export {auth,db};
+export { auth, db };
